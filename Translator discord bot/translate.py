2a0e67d086adfae566be886362b7e0bf12c817a9
jandrews4 <78866714+jandrews4@users.noreply.github.com>
2021-06-06 15:22:36 -0600

    Delete translate.py

--- Translator discord bot/translate.py
+++
@@ -1,9 +0,0 @@
-import google_trans_new
-import requests
-from google_trans_new import google_translator
-
-translator = google_translator()
-text = "lava los platos"
-
-def translate(text):
-    return(translator.translate(text, lang_tgt="en"))
